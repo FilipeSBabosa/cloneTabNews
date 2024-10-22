438b8bee1af98f30fe6e9a54e3ea7419f8cfe07c
Filipe Barbosa <144185319+FilipeSBabosa@users.noreply.github.com>
2024-10-22 00:22:23 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>não sei o que escrever</h1>
+  return <h1>não sei o que escrever</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
